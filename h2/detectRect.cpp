fa80610df3c70b1c09782921a052202febc45571
Fujun Du <fujun.du@gmail.com>
2014-10-02 14:56:21 -0400

    Finished

---
+++ h2/detectRect.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#define INT int
+
+void swap(INT *x, INT*y) {
+    INT t=*x;
+    *x = *y;
+    *y = t;
+}
+
+void sorted(INT *nx, INT *ny, int *ind, int n1, int n2) {
+    if (n1 >= n2) {
+        return;
+    }
+    int imid = (n1+n2)>>1;
+    //printf("%d,%d,%d\n", imid, n1, n2);
+    INT pvtx = nx[imid], pvty = ny[imid];
+    swap(&nx[imid], &nx[n2]);
+    swap(&ny[imid], &ny[n2]);
+    swap(&ind[imid], &ind[n2]);
+    int idx = n1-1;
+    for (int i=n1; i<n2; i++) {
+        if ((nx[i] < pvtx) || \
+            ((nx[i] == pvtx) && (ny[i] < pvty))) {
+            idx ++;
+            swap(&nx[i], &nx[idx]);
+            swap(&ny[i], &ny[idx]);
+            swap(&ind[i], &ind[idx]);
+        }
+    }
+    idx++;
+    swap(&nx[idx], &nx[n2]);
+    swap(&ny[idx], &ny[n2]);
+    swap(&ind[idx], &ind[n2]);
+    sorted(nx, ny, ind, n1, idx-1);
+    sorted(nx, ny, ind, idx+1, n2);
+}
+
+bool has_rect(INT *mx, INT *my, INT *nx, int *ny, int *ind, int n1, int n2) {
+    if ((n1+3) > n2) {
+        return false;
+    }
+    for (int i=n1+1; i<=n2; i++) {
+        if (my[i] != my[n1]) {
+            return has_rect(mx, my, nx, ny, ind, i, n2);
+        }
+        int i0 = ind[n1];
+        for (int j=i0; j<=n2; j++) {
+            if (nx[j] != nx[i0]) {
+                return has_rect(mx, my, nx, ny, ind, n1+1, n2);
+            }
+            int j0 = ind[i];
+            for (int k=j0; k<=n2; k++) {
+                if (nx[k] != nx[j0]) {
+                    break;
+                } else if (ny[j] == ny[k]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main(int argc, char *argv[]){
+    string line = "";
+    std::getline(std::cin, line, '\n');
+    std::stringstream lineStream(line);
+    int i;
+    std::vector<int> values;
+    while (lineStream >> i) {
+        values.push_back(i);
+    }
+    int n = values.size();
+    if (n < 8) {
+        printf("No\n");
+        return 1;
+    }
+    int np = n/2;
+    INT *nx, *ny, *mx, *my, *ind;
+    nx = new INT[np];
+    ny = new INT[np];
+    mx = new INT[np];
+    my = new INT[np];
+    ind = new int[np];
+    for (int i=0; i<np; i++) {
+        mx[i] = values[2*i];
+        my[i] = values[2*i+1];
+    }
+    sorted(my, mx, ind, 0, np-1);
+    for (int i=0; i<np; i++) {
+        nx[i] = mx[i];
+        ny[i] = my[i];
+        ind[i] = i;
+    }
+    sorted(nx, ny, ind, 0, np-1);
+    //
+    //for (int i=0; i<np; i++) {
+    //    printf("%d, %d\n", nx[i], ny[i]);
+    //}
+    //printf("\n%d,%d,%d\n\n", 0>>1, 6>>1, (0>>1)+(6>>1));
+    //for (int i=0; i<np; i++) {
+    //    printf("%d, %d\n", mx[i], my[i]);
+    //}
+    //
+    bool has_re = has_rect(mx, my, nx, ny, ind, 0, np-1);
+    if (has_re) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+    return 0;
+}
---
+++ h2/maxSubSum.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+#define INT int
+
+
+INT calc_max_sum(INT *ns, int n1, int n2) {
+    if (n1 >= n2) {
+        return ns[n2];
+    }
+    if (ns[n1] >= 0) {
+        INT r, s, t;
+        s = ns[n1];
+        int i, j;
+        for (i=n1+1; i<=n2; i++) {
+            if (ns[i] >= 0) {
+                s += ns[i];
+            } else {
+                break;
+            }
+        }
+        t = 0;
+        for (j=i; j<=n2; j++) {
+            if (ns[j] <= 0) {
+                t += ns[j];
+            } else {
+                break;
+            }
+        }
+        r = calc_max_sum(ns, j, n2);
+        return max(max(s, s+t+r), r);
+    } else {
+        INT s = ns[n1];
+        int i;
+        for (i=n1+1; i<=n2; i++) {
+            s = max(s, ns[i]);
+            if (ns[i] > 0) {
+                break;
+            }
+        }
+        return max(s, calc_max_sum(ns, i, n2));
+    }
+}
+
+
+int main(int argc, char *argv[]){
+    INT *ns;
+    string line = "";
+    std::getline(std::cin, line, '\n');
+    std::stringstream lineStream(line);
+    int i;
+    std::vector<int> values;
+    while (lineStream >> i) {
+        values.push_back(i);
+    }
+    int n = values.size();
+    ns = new INT[n];
+    for (int i=0; i<n; i++) {
+        ns[i] = values[i];
+    }
+    if (n <= 0) {
+        printf("%d\n", 0);
+    } else {
+        printf("%d\n", calc_max_sum(ns, 0, n-1));
+    }
+}
