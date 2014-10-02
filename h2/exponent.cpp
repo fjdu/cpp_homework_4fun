a6a5f3c1e7a4e9bf430c2425f1c78696b9c56d84
Fujun Du <fujun.du@gmail.com>
2014-10-02 16:53:26 -0400

    Small change

--- h2/exponent.cpp
+++ h2/exponent.cpp
@@ -21,7 +21,12 @@ INT calc_last_4digit(INT a, INT n) {
     }
     a = a % MODBASE;
     if (n%2 == 0) {
-        return calc_last_4digit((a*a) % MODBASE, n>>1);
+        INT b = (a*a) % MODBASE;
+        if (a != b) {
+            return calc_last_4digit(b, n>>1);
+        } else {
+            return a;
+        }
     } else {
         return (a*calc_last_4digit(a, n-1)) % MODBASE;
     }
@@ -29,28 +34,16 @@ INT calc_last_4digit(INT a, INT n) {
 
 
 INT calc_last_4digit_list(INT *ns, int n1, int n2) {
-    if (n1+1 == n2) {
-        return calc_last_4digit(ns[n1], ns[n2]);
-    } else {
-        return calc_last_4digit(calc_last_4digit_list(ns, n1, n2-1), ns[n2]);
+    INT r = ns[n1];
+    for (int i=n1+1; i<=n2; i++) {
+        r = calc_last_4digit(r, ns[i]);
     }
+    return r;
 }
 
 
 int main(int argc, char *argv[]){
     INT *ns;
-    //
-    //char s_in[NMAXIN];
-    //int n = argc - 1;
-    //if (n < 2) {
-    //    printf("At least two arguments needed!\n");
-    //    return 1;
-    //}
-    //ns = new INT[n];
-    //for (int i=0; i<n; i++) {
-    //    ns[i] = atoi(argv[i+1]);
-    //}
-    //
     // http://stackoverflow.com/questions/1145831/get-list-of-numbers-from-stdin-and-tokenize-them
     string line = "";
     std::getline(std::cin, line, '\n');
@@ -65,5 +58,9 @@ int main(int argc, char *argv[]){
     for (int i=0; i<n; i++) {
         ns[i] = values[i];
     }
-    printf("%d\n", calc_last_4digit_list(ns, 0, n-1));
+    if (n==1) {
+        printf("%d\n", ns[0]%MODBASE);
+    } else {
+        printf("%d\n", calc_last_4digit_list(ns, 0, n-1));
+    }
 }
