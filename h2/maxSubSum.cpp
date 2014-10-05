a0c04058d2da730f75861185ea29f89d2593dc71
Fujun Du <fujun.du@gmail.com>
2014-10-04 21:56:32 -0400

    Removed unneeded stuff

--- h2/maxSubSum.cpp
+++ h2/maxSubSum.cpp
@@ -7,25 +7,9 @@
 
 using namespace std;
 
-
 #define INT int
 
 //#define NAIVE
-
-INT calc_max_sum_enum(INT *ns, int n1, int n2) {
-    // Naive approach.
-    INT s;
-    s = ns[n1];
-    for (int i=n1; i<=n2; i++) {
-        INT t = ns[i];
-        for (int j=i+1; j<=n2; j++) {
-            t += ns[j];
-            s = max(s, t);
-        }
-    }
-    return s;
-}
-
 
 INT partial_sum_L(INT *ns, int n1, int n2) {
     INT s = ns[n2];
@@ -71,49 +55,25 @@ INT calc_max_sum_plus_minus(INT *ns, int n1, int n2) {
 }
 
 
-void reduce_plus_minus(INT *ns, int n, int *nseg) {
-    int ibg = 0;
-    int ied = n;
-    if (ns[0] <= 0) {
-        ibg = n + 1;
-        for (int i=1; i<=n; i++) {
-            if (ns[i] > 0) {
-                ibg = i;
-                break;
-            }
-        }
-    }
-    if (ns[n] <= 0) {
-        ied = -1;
-        for (int i=n-1; i>=0; i--) {
-            if (ns[i] > 0) {
-                ied = i;
-                break;
-            }
-        }
-    }
-    if ((ibg > n) || (ied < 0)) {
-        *nseg = -1;
-        return;
-    }
-    *nseg = 0;
-    ns[*nseg] = ns[ibg];
-    for (int i=ibg+1; i<=ied; i++) {
-        //printf("%d,%d\n", *nseg, i);
-        if (ns[i] * ns[i-1] < 0) {
-            *nseg += 1;
-            ns[*nseg] = ns[i];
-        } else {
-            ns[*nseg] += ns[i];
-        }
-    }
-    return;
-}
-
 INT get_max(INT *ns, int n) {
     INT s = ns[0];
     for (int i=1; i<=n; i++) {
         s = max(s, ns[i]);
+    }
+    return s;
+}
+
+
+INT calc_max_sum_enum(INT *ns, int n1, int n2) {
+    // Naive approach.
+    INT s;
+    s = ns[n1];
+    for (int i=n1; i<=n2; i++) {
+        INT t = ns[i];
+        for (int j=i+1; j<=n2; j++) {
+            t += ns[j];
+            s = max(s, t);
+        }
     }
     return s;
 }
@@ -145,16 +105,10 @@ int main(int argc, char *argv[]){
             printf("%d\n", get_max(ns, n-1));
             return 0;
         }
-        int nseg;
-        reduce_plus_minus(ns, n-1, &nseg);
 #ifdef NAIVE
-        printf("%d\n", calc_max_sum_enum(ns, 0, nseg));
+        printf("%d\n", calc_max_sum_enum(ns, 0, n-1));
 #else
-        if (nseg < 0) {
-            printf("%d\n", 0);
-            return 0;
-        }
-        printf("%d\n", calc_max_sum_plus_minus(ns, 0, nseg));
+        printf("%d\n", calc_max_sum_plus_minus(ns, 0, n-1));
 #endif
     }
 }
