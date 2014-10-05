cc43a6105b255699238bc7b9922f53c8eeb34e78
Fujun Du <fujun.du@gmail.com>
2014-10-04 21:41:19 -0400

    Divide and conquer
    Tested with the brute force approach.
    
    If all the input values are negative, will output the maximum.

--- h2/maxSubSum.cpp
+++ h2/maxSubSum.cpp
@@ -10,61 +10,116 @@ using namespace std;
 
 #define INT int
 
+//#define NAIVE
 
-INT calc_max_sum(INT *ns, int n1, int n2) {
+INT calc_max_sum_enum(INT *ns, int n1, int n2) {
+    // Naive approach.
+    INT s;
+    s = ns[n1];
+    for (int i=n1; i<=n2; i++) {
+        INT t = ns[i];
+        for (int j=i+1; j<=n2; j++) {
+            t += ns[j];
+            s = max(s, t);
+        }
+    }
+    return s;
+}
+
+
+INT partial_sum_L(INT *ns, int n1, int n2) {
+    INT s = ns[n2];
+    INT t = s;
+    for (int i=n2-1; i>=n1; i--) {
+        s += ns[i];
+        t = max(t, s);
+    }
+    return t;
+}
+
+
+INT partial_sum_R(INT *ns, int n1, int n2) {
+    INT s = ns[n1];
+    INT t = s;
+    for (int i=n1+1; i<=n2; i++) {
+        s += ns[i];
+        t = max(t, s);
+    }
+    return t;
+}
+
+
+INT calc_max_sum_plus_minus(INT *ns, int n1, int n2) {
     if (n1 >= n2) {
         return ns[n2];
     }
-    if (ns[n1] >= 0) {
-        INT r, s, t;
-        s = ns[n1];
-        int i, j;
-        for (i=n1+1; i<=n2; i++) {
-            if (ns[i] >= 0) {
-                s += ns[i];
-            } else {
-                break;
-            }
-        }
-        if ((i<=n2) && (ns[i] < 0)) {
-            t = 0;
-            for (j=i; j<=n2; j++) {
-                if (ns[j] <= 0) {
-                    t += ns[j];
-                } else {
-                    break;
-                }
-            }
-            if ((j<=n2) && (ns[j] > 0)) {
-                r = calc_max_sum(ns, j, n2);
-                return max(max(s, s+t+r), r);
-            } else {
-                return s;
-            }
-        } else {
-            return s;
-        }
-    } else {
-        INT s = ns[n1];
-        int i;
-        for (i=n1+1; i<=n2; i++) {
-            s = max(s, ns[i]);
-            if (ns[i] > 0) {
-                break;
-            }
-        }
-        return max(s, calc_max_sum(ns, i, n2));
+    if (n1+1 == n2) {
+        return max(max(ns[n1], ns[n2]), ns[n1]+ns[n2]);
     }
+    int nmid;
+    nmid = (n1 + n2) >> 1;
+    INT s1 = calc_max_sum_plus_minus(ns, n1, nmid-1);
+    INT s2 = calc_max_sum_plus_minus(ns, nmid+1, n2);
+    INT sL = partial_sum_L(ns, n1, nmid-1);
+    INT sR = partial_sum_R(ns, nmid+1, n2);
+    s1 = max(s1, s2);
+    s1 = max(s1, ns[nmid]);
+    s1 = max(s1, sL+ns[nmid]);
+    s1 = max(s1, sR+ns[nmid]);
+    s1 = max(s1, sL+sR+ns[nmid]);
+    return s1;
+}
+
+
+void reduce_plus_minus(INT *ns, int n, int *nseg) {
+    int ibg = 0;
+    int ied = n;
+    if (ns[0] <= 0) {
+        ibg = n + 1;
+        for (int i=1; i<=n; i++) {
+            if (ns[i] > 0) {
+                ibg = i;
+                break;
+            }
+        }
+    }
+    if (ns[n] <= 0) {
+        ied = -1;
+        for (int i=n-1; i>=0; i--) {
+            if (ns[i] > 0) {
+                ied = i;
+                break;
+            }
+        }
+    }
+    if ((ibg > n) || (ied < 0)) {
+        *nseg = -1;
+        return;
+    }
+    *nseg = 0;
+    ns[*nseg] = ns[ibg];
+    for (int i=ibg+1; i<=ied; i++) {
+        //printf("%d,%d\n", *nseg, i);
+        if (ns[i] * ns[i-1] < 0) {
+            *nseg += 1;
+            ns[*nseg] = ns[i];
+        } else {
+            ns[*nseg] += ns[i];
+        }
+    }
+    return;
+}
+
+INT get_max(INT *ns, int n) {
+    INT s = ns[0];
+    for (int i=1; i<=n; i++) {
+        s = max(s, ns[i]);
+    }
+    return s;
 }
 
 
 int main(int argc, char *argv[]){
-    //{
-    //    int i;
-    //    for (i=0; i<=2; i++) {if (i==2) break;};
-    //    printf("%d\n", i);
-    //    return 0;
-    //}
     INT *ns;
     string line = "";
     std::getline(std::cin, line, '\n');
@@ -75,13 +130,31 @@ int main(int argc, char *argv[]){
         values.push_back(i);
     }
     int n = values.size();
+    bool all_negative = true;
     ns = new INT[n];
     for (int i=0; i<n; i++) {
         ns[i] = values[i];
+        if (ns[i] > 0) {
+            all_negative = false;
+        }
     }
     if (n <= 0) {
         printf("%d\n", 0);
     } else {
-        printf("%d\n", calc_max_sum(ns, 0, n-1));
+        if (all_negative) {
+            printf("%d\n", get_max(ns, n-1));
+            return 0;
+        }
+        int nseg;
+        reduce_plus_minus(ns, n-1, &nseg);
+#ifdef NAIVE
+        printf("%d\n", calc_max_sum_enum(ns, 0, nseg));
+#else
+        if (nseg < 0) {
+            printf("%d\n", 0);
+            return 0;
+        }
+        printf("%d\n", calc_max_sum_plus_minus(ns, 0, nseg));
+#endif
     }
 }
