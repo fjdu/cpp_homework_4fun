4abaae0e8e756d9d77f0d7d90ad18bccb24f5e4e
Fujun Du <fujun.du@gmail.com>
2014-10-20 17:44:25 -0400

    h3: 1st finished

---
+++ h3/knapsack.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#define NOT_YET_CALCULATED -1
+#define NO_SOLUTION -2
+
+#define EXACT_MATCH
+
+int ks(int *v, int *w, int *s, int n, int W, int WL, int iL) {
+    if (iL == 0) {
+#ifdef EXACT_MATCH
+        if (w[0] == WL) {
+            return v[0];
+        } else {
+            return NO_SOLUTION;
+        }
+#else
+        if (w[0] <= WL) {
+            return v[0];
+        } else {
+            return 0;
+        }
+#endif
+    }
+    if (WL < 1) {
+        return 0;
+    }
+    int idx = iL*W + WL - 1;
+    if (s[idx] != NOT_YET_CALCULATED) {
+        return s[idx];
+    }
+    if (WL >= w[iL]) {
+        int r = ks(v, w, s, n, W, WL-w[iL], iL-1);
+        int r1;
+        if (r != NO_SOLUTION) {
+            r1 = r + v[iL];
+        } else {
+            r1 = NO_SOLUTION;
+        }
+        int r2 = ks(v, w, s, n, W, WL, iL-1);
+        s[idx] = max(r1, r2);
+    } else {
+        s[idx] = ks(v, w, s, n, W, WL, iL-1);
+    }
+    return s[idx];
+}
+
+
+int gcd(int u, int v)
+//http://en.wikipedia.org/wiki/Binary_GCD_algorithm
+{
+    // simple cases (termination)
+    if (u == v)
+        return u;
+ 
+    if (u == 0)
+        return v;
+ 
+    if (v == 0)
+        return u;
+ 
+    // look for factors of 2
+    if (~u & 1) // u is even
+    {
+        if (v & 1) // v is odd
+            return gcd(u >> 1, v);
+        else // both u and v are even
+            return gcd(u >> 1, v >> 1) << 1;
+    }
+ 
+    if (~v & 1) // u is odd, v is even
+        return gcd(u, v >> 1);
+ 
+    // reduce larger argument
+    if (u > v)
+        return gcd((u - v) >> 1, v);
+ 
+    return gcd((v - u) >> 1, u);
+}
+
+
+int main(int argc, char *argv[]){
+    string line = "";
+    std::getline(std::cin, line, '\n');
+    std::stringstream lineStream(line);
+    int i;
+    std::vector<int> values;
+    while (lineStream >> i) {
+        values.push_back(i);
+    }
+    //
+    int n0 = values.size();
+    int W = values[0], n = values[1];
+    if (n*2 != (n0-2)) {
+        return 1;
+    }
+    //printf("%d; %d\n", W, n);
+    int *v, *w;
+    v = new int[n];
+    w = new int[n];
+    int g = W;
+    for (i=0; i<n; i++) {
+        v[i] = values[2+2*i];
+        w[i] = values[2+2*i+1];
+        g = gcd(g, w[i]);
+        //printf("%d, %d\n", v[i], w[i]);
+    }
+    if (g > 1) {
+        W /= g;
+        for (i=0; i<n; i++) {
+            w[i] /= g;
+        }
+    }
+    //
+    int *s;
+    s = new int[n*W];
+    for (i=0; i<n*W; i++) {
+        s[i] = NOT_YET_CALCULATED;
+    }
+    printf("%d\n", g*max(0, ks(v, w, s, n, W, W, n-1)));
+    return 0;
+}
