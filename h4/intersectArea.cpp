21a632838b68ff77c212973a8682a65b873da86b
Fujun Du <fujun.du@gmail.com>
2014-11-06 22:22:21 -0500

    updated

--- h4/intersectArea.cpp
+++ h4/intersectArea.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 #define EPS 1e-10
-#define PI 3.14159265
+#define PI 3.141592653589793
 
-void inoutside_unit_circle(float x1, float y1, float x2, float y2, \
+void inoutside_unit_circle(double x1, double y1, double x2, double y2, \
                            bool *is_outside, bool *is_inside, bool *contain_circ) {
-    float r11 = x1*x1 + y1*y1;
-    float r12 = x1*x1 + y2*y2;
-    float r21 = x2*x2 + y1*y1;
-    float r22 = x2*x2 + y2*y2;
+    double r11 = x1*x1 + y1*y1;
+    double r12 = x1*x1 + y2*y2;
+    double r21 = x2*x2 + y1*y1;
+    double r22 = x2*x2 + y2*y2;
     bool outside_1 = \
                   (r11 >  1.0) && \
                   (r12 >  1.0) && \
@@ -38,7 +38,10 @@ void inoutside_unit_circle(float x1, float y1, float x2, float y2, \
 }
 
 
-float cArea(float x1, float y1, float x2, float y2) {
+double cArea(double x1, double y1, double x2, double y2) {
+    if ((x2 <= x1) || (y2 <= y1)) {
+        return 0.0;
+    }
     bool is_out, is_in, is_contain;
     inoutside_unit_circle(x1, y1, x2, y2, &is_out, &is_in, &is_contain);
     if (is_in) {
@@ -50,8 +53,8 @@ float cArea(float x1, float y1, float x2, float y2) {
     } else if ((x2-x1)*(y2-y1) < EPS) {
         return 0.0;
     } else {
-        float xmid = 0.5 * (x1 + x2);
-        float ymid = 0.5 * (y1 + y2);
+        double xmid = 0.5 * (x1 + x2);
+        double ymid = 0.5 * (y1 + y2);
         return cArea(x1, y1, xmid, ymid) + \
                cArea(x1, ymid, xmid, y2) + \
                cArea(xmid, y1, x2, ymid) + \
@@ -64,12 +67,12 @@ int main(int argc, char *argv[]){
         return 1;
     }
     //
-    float x1 = atof(argv[1]);
-    float y1 = atof(argv[2]);
-    float x2 = atof(argv[3]) - x1;
-    float y2 = atof(argv[4]) - y1;
-    float x3 = atof(argv[5]) - x1;
-    float y3 = atof(argv[6]) - y1;
+    double x1 = atof(argv[1]);
+    double y1 = atof(argv[2]);
+    double x2 = atof(argv[3]) - x1;
+    double y2 = atof(argv[4]) - y1;
+    double x3 = atof(argv[5]) - x1;
+    double y3 = atof(argv[6]) - y1;
     //
     printf("%.4f\n", cArea(x2, y2, x3, y3));
     return 0;
